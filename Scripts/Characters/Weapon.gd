39d4ef96d0a965cc495cba113aa39f7f73bdabc4
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2024-02-26 15:25:34 +0100

    feat: make weapons into own scene

--- Scripts/Characters/Weapon.gd
+++ Scripts/Characters/Weapon.gd
@@ -1,5 +1,23 @@
 extends Area2D
 
+@export var damage := 1
 
-func _process(delta):
-	pass
+@onready var animation_player = $AnimationPlayer
+
+
+func _ready():
+	connect("body_entered", _body_entered)
+
+
+func _body_entered(node):
+	print("hit " + node.name)
+	if node.is_in_group("enemies"):
+		node.hit(damage)
+	elif node.is_in_group("interactable"):
+		node.interact()
+
+
+func _input(event):
+	if event.is_action_pressed("primary_attack"):
+		print("attacking")
+		animation_player.current_animation = "SwingWeapon"
--- Scripts/State Machine/Goblin/Goblin.gd
+++ Scripts/State Machine/Goblin/Goblin.gd
@@ -6,10 +6,19 @@ extends CharacterBody2D
 @onready var state_machine = $"State Machine"
 @onready var damage_area_2d = $DamageArea2D
 
+@onready var health = $Health
+
 
 func _ready():
 	damage_area_2d.connect("damage_done", damage_done)
 	state_machine.connect("state_changed", state_changed)
+	health.connect("health_changed", health_changed)
+	health.connect("died", died)
+
+
+func hit(damage):
+	print("hit recieved")
+	health.damage(damage)
 
 
 func _physics_process(delta):
@@ -21,6 +30,17 @@ func state_changed(from, to):
 	set_collision_mask_value(2, !is_stunned)
 	set_collision_layer_value(2, !is_stunned)
 	damage_area_2d.set_deferred("monitoring", !is_stunned)
+
+
+func health_changed(health):
+	damage_done({
+		"to": get_tree().get_nodes_in_group("player")[0],
+		"by": self
+	})
+
+
+func died():
+	queue_free()
 
 
 func damage_done(damage):
